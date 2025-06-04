92c47e33669f5107893959b88d9a169476f847ea
Xavier Bester <xavierbester6@gmail.com>
2025-06-04 17:48:45 -0600

    got the first little basics of main to work

--- tasks.go
+++ tasks.go
@@ -1,1 +1,41 @@
 package todo
+
+import (
+	"time"
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) > 0 && len(args) <= 2 {
+		if args[0] == 
+	}
+
+	fmt.Println("The correct usage is: go run . $argument/flag $task/nothing")
+}
+
+func list() string{
+
+}
+
+func add(task string) {
+
+}
+
+func delete(task string) {
+
+}
+
+func complete(task string) {
+
+}
+
+func loader(filePath string) {
+
+}
+
+func writer(filePath string) {
+
+}
